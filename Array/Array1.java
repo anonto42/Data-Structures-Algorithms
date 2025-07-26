c256e281ba110240bd1bfe28ca9f121c80a2689d
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-26 21:53:12 +0600

    first video ended

--- Array/Array1.java
+++ Array/Array1.java
@@ -3,5 +3,14 @@ public class Array1{
         
         int[] arr = new int[4];
         
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
     }
 }
